87620ca04742b21ca24d7c3e33dee94343dcdee1
wezendy <vszabi98@gmail.com>
2022-11-04 19:04:39 +0100

    Adding the game

---
+++ Assets/Scripts/Car_Move.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+public class Car_Move : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform car;
+    public float speed = 17;
+
+
+    Vector3 rotationRight = new Vector3(0, 60, 0);
+    Vector3 rotationLeft = new Vector3(0, -60, 0);
+
+    Vector3 forward = new Vector3(0, 0, 1);
+    Vector3 backward = new Vector3(0, 0, -1);
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(backward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotationRight);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotationLeft);
+        }
+
+    }
+}
