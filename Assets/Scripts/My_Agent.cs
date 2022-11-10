ff3ed16bcbd8fcddfe6d678a913be37be6f2a545
Jakab Bernát <71104003+bjakab295@users.noreply.github.com>
2022-11-10 15:06:36 +0100

    MLAgent
    Resolved issue with MLAgents support

---
+++ Assets/Scripts/My_Agent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class My_Agent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
